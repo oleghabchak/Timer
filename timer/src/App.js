bc7916f0c0fbd495726abbc6f087e21155097808
oleghabchak <71442227+oleghabchak@users.noreply.github.com>
2021-10-06 03:26:03 +0300

    create layout and work with css

--- timer/src/App.js
+++ timer/src/App.js
@@ -8,14 +8,44 @@ const data = {
 
 function Timer({ interval }) {
   const duration = moment.duration(interval)
-  return  <span className='timer'>{ interval}</span>
+  return  <div className='timer'>
+      <p>
+        { interval}
+      </p>
+    </div>
 }
 
+function Buttons({ color, title, background}) {
+  return(
+    <div className="buttBorder">
+      <div className='button' style={{backgroundColor: background}}>
+        <p style={{color}}>{title}</p>
+      </div>
+    </div>
+  );
+}
+
+function LapList() {
+  return(
+    <div className="lapTable">
+      <h2>lap1</h2>
+      <h2>lap2</h2>
+      <h2>lap3</h2>
+    </div>
+  )
+  
+}
 export default class App extends Component {
   render() {
     return(
     <div className="container">
       <Timer interval={data.timer} />
+      <div className="row">
+          <Buttons color='#50D166' title="Start" background='#1B361F'/>
+          <Buttons color='#FFF' title="Reset"  background='#333333' />
+      </div>
+      
+      <LapList/>
     </div>
       )
   }
